00f67fd3d951115fe590e3a64355ce949e416bb4
Iley Zakhvatkin <iley.zakhvatkin@yandex.ru>
2023-11-03 19:18:16 +0300

    Auth Success

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -12,8 +12,6 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
-//use Symfony\Component\Security\Core\Security;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
@@ -27,6 +25,9 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
     use TargetPathTrait;
 
     public const LOGIN_ROUTE = 'app_login';
+
+    public const LAST_USERNAME = '_security.last_username';
+    public const AUTHENTICATION_ERROR = '_security.last_error';
 
     /**
      * @var UrlGeneratorInterface
@@ -57,14 +58,12 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
     public function authenticate(Request $request): Passport
     {
         $email = $request->request->get('email');
-        $user = $this->userRepository->findOneBy(['email' => $email]);
 
-        if( !empty($user) ){
-            throw new CustomUserMessageAuthenticationException('Пользователя не существует в системе');
+        if ( $email === 'admin@exmaple.com' ) {
+            throw new CustomUserMessageAuthenticationException('Убирайся, ты нам не нравишься!');
         }
 
-        // До сих пор работает
-        $request->getSession()->set(Security::LAST_USERNAME, $email);
+        $request->getSession()->set(self::LAST_USERNAME, $email);
 
         return new Passport(
             new UserBadge($email),
@@ -79,7 +78,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
     {
         $response = new Response();
-        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+        $request->getSession()->set(self::AUTHENTICATION_ERROR, $exception);
 
         return new RedirectResponse('/login');
     }
